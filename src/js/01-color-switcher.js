af4f6b1e23f9a8f47c0ec7b99eb187ad1eb33c23
Olexandr Horbenko <horbenko.o.a@gmail.com>
2023-03-25 07:41:56 +0200

    Do task 1 color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,43 @@
+import { refs } from './01-color-switcher';
 
+export function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+export function setBodyBgColor(color) {
+  document.body.style.backgroundColor = color;
+}
+
+export function setColorSwitchState(state) {
+  refs.startBtn.disabled = state;
+  refs.stopBtn.disabled = !state;
+}
+
+export const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+
+let colorSwitchIntervalId = null;
+let currentColor = null;
+
+setColorSwitchState(false);
+
+refs.startBtn.addEventListener('click', startColorSwitch);
+refs.stopBtn.addEventListener('click', stopColorSwitch);
+
+function startColorSwitch() {
+  setColorSwitchState(true);
+
+  colorSwitchIntervalId = setInterval(() => {
+    currentColor = getRandomHexColor();
+    setBodyBgColor(currentColor);
+  }, 1000);
+}
+
+function stopColorSwitch() {
+  setColorSwitchState(false);
+
+  clearInterval(colorSwitchIntervalId);
+  if (currentColor) setBodyBgColor(currentColor);
+}
